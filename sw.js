aabb8f3d5d21e529f3d86e46d7b24df9a228c9d8
matiusansell <c14200064@john.petra.ac.id>
2023-03-31 08:50:09 +0700

    UTS

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
       .open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => cache.addAll(URLS))
       .then(() => self.skipWaiting())
   );
 });
